e64981f7e6e0fb70876913f94e44ee6ef3f7d8bb
Suchendra <suchendrasharma1814@gmail.com>
2021-04-11 10:05:01 +0530

    TODO list added

---
+++ src/app/todo/todo.component.ts
@@ -0,0 +1,58 @@
+import { Component, OnInit } from '@angular/core';
+import * as Aos from 'aos';
+import { from } from 'rxjs';
+import { Todo } from '../models/todo';
+
+@Component({
+  selector: 'app-todo',
+  templateUrl: './todo.component.html',
+  styleUrls: ['./todo.component.scss']
+})
+export class TodoComponent implements OnInit {
+
+  todo:Todo[];
+
+
+  inputTodo:string = "";
+
+  constructor() {
+    this.todo= [];
+   }
+
+  ngOnInit(): void {
+
+    Aos.init();
+    
+    this.todo = [
+      {
+        content: 'First todo',
+        completed: false
+      },
+      {
+        content: 'Second todo',
+        completed: false
+      }
+    ]
+  }
+  
+  toggleDone (id:number) {
+    this.todo.map((v, i) => {
+      if (i == id) v.completed = !v.completed;
+
+      return v;
+    })
+  }
+  deleteTodo (id:number) {
+    this.todo = this.todo.filter((v, i) => i !== id);
+  }
+
+  addTodo () {
+    this.todo.push({
+      content: this.inputTodo,
+      completed: false
+    });
+
+    this.inputTodo = "";
+  }
+
+}
